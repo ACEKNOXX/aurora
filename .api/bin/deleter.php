76e515d89d7d07ca84fde37a072e823a6253db1e
Aceknoxx <mustapha.ddare@gmail.com>
2020-11-18 13:18:12 +0000

    api

---
+++ .api/bin/deleter.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: adebola
+ * Date: 11/20/2014
+ * Time: 12:28 AM
+ */
+
+class deleter {
+    var $conn;
+    function __construct($conn)
+    {
+        $this->conn = $conn;
+    }
+
+} 
